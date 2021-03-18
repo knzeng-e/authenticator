1021523d0a78d65d2959ef529faec36576e0d0f5
knzeng-e <kevin.nzeng@gmail.com>
2021-03-18 22:53:58 +0100

    feat(index.js: setting the connection to the MongoDB dataBase

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app = express()
+const app = require('express')();
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+//import routes
+
+const authRoutes = require('./routes/auth/auth');
+
+//connection to the database
+
+mongoose.connect(
+    process.env.MONGODB_CONNECT,
+    { useNewUrlParser : true, useUnifiedTopology: true},
+    () => {
+        console.log("Connected to the database ...")
+    }
+    )
 
 const PORT = process.env.PORT || 4242
 
 app.get('/', (req, res) => {
-    console.log('Nw request done to the server ==> ', req)
+    console.log('New request done to the server ==> ')
     res.send("Hello Backend World")
 })
 
